cdbd78256f02859d95c5801c3d0ef53e223bc0a7
koltok <margarita.koltok@gmail.com>
2024-02-29 12:26:30 +0200

    Initial commit

---
+++ js/gallery.js
@@ -0,0 +1,134 @@
+const gal = document.querySelector(".gallery");
+
+const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
+];
+
+const galEl = images.map((im) => {
+    const item = document.createElement("li");
+    item.classList.add("gallery-item");
+    gal.append(item);
+    const link = document.createElement("a");
+    link.classList.add("gallery-link");
+    link.href = im.original;
+    item.append(link);
+    const image = document.createElement("img");
+    image.classList.add("gallery-image");
+    image.src = im.preview;
+    image.alt = im.description;
+    image.dataset.source = im.original;
+    link.append(image);
+}
+)
+
+gal.addEventListener("click", function (event) {
+    console.log(event.target.dataset.source);
+    
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+       <img class = "foto" />
+        <a></a>
+    </div>
+`, {
+    onShow: (instance) => {
+        instance.element().querySelector('a').onclick = instance.close
+    }
+})
+
+    instance.show()
+
+  
+    const modalForm = document.querySelector(".modal");
+    const modalFoto = document.querySelector(".foto");
+    modalFoto.src = event.target.dataset.source;
+    modalFoto.alt = event.target.alt;
+    //image.append(modalForm);
+});
+
+
+//const galEl = images.map((im) => '<li class="gallery-item"><a class="gallery-link" href="large-image.jpg"><img class="gallery-image" src = "im.preview" data - source="${im.original}" alt = "im.description" /> </a> </li>')
+//.join("");
+
+//gal.insertAdjacentHTML("afterbegin", galEl);
+   
+
+//const item = document.createElement("li");
+//item.classList.add("gallery-item");
+//gal.insertAdjacentHTML("afterbegin", item);
+//item.setAttribute("", "");
+//const link = document.createElement("a");
+//link.classList.add("gallery-link");
+//item.setAttribute("", "");
+//const image = document.createElement("img");
+//image.classList.add("gallery-image");
+//item.setAttribute("", "");
+
+
+
+
+
+
+
